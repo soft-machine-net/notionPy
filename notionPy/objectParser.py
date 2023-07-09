a783c9fbcf4476eb891efd5530693ac4a3152450
tamura <tamura.naoya.san@gmail.com>
2023-07-09 19:10:21 +0900

    objectParser change class->method

--- notionPy/objectParser.py
+++ notionPy/objectParser.py
@@ -2,55 +2,42 @@ import json
 from . import parsers
 
 
-class objectParser:
-    @staticmethod
-    def propertyToText(property):
-        property_type = property['type']
-        handler = getattr(parsers.property, f"handle_{property_type}", parsers.property.handle_unsupport_property(property))
-        return handler(property)
 
-    @staticmethod
-    def blockToText(block, isMarkdown=False):
-        block_type = block["type"]
-        handler = getattr(parsers.block , f"handle_{block_type}", parsers.block.handle_unsupport_block(block))
-        return handler(block, isMarkdown)
+def propertyToText(property):
+    property_type = property['type']
+    handler = getattr(parsers.property, f"handle_{property_type}", parsers.property.handle_unsupport_property(property))
+    return handler(property)
 
-    @staticmethod
-    def childrenToText(children, isMarkdown = False):
-        text = ""
-        for child in children['results']:
-            text += objectParser.blockToText(child, isMarkdown) + "\n"
-        return text
+def blockToText(block, isMarkdown=False):
+    block_type = block["type"]
+    handler = getattr(parsers.block , f"handle_{block_type}", parsers.block.handle_unsupport_block(block))
+    return handler(block, isMarkdown)
 
-    @staticmethod
-    def pageToText(page, isContainMetadata=False, properties = None ):
-        # Metadata
-        metadata_text = ""
-        if isContainMetadata:
-            metadata_text += f"Page URL: {page['url']}\n"
-            metadata_text += f"Created Time: {page['created_time']}\n"
-            metadata_text += f"Last Edited Time: {page['last_edited_time']}\n"
-            metadata_text += f"Created By: {page['created_by']['id']}\n"
-            metadata_text += f"Last Edited By: {page['last_edited_by']['id']}\n"
-            metadata_text += f"Archived: {page['archived']}\n"
-            metadata_text += f"Parent: {page['parent']['type']}\n"
-            metadata_text += f"Cover: {page['cover']['url']}\n" if page['cover'] != None else ""
+def childrenToText(children, isMarkdown = False):
+    text = ""
+    for child in children['results']:
+        text += blockToText(child, isMarkdown) + "\n"
+    return text
 
-        properties_text = ""
-        for prop_name, prop_value in page['properties'].items():
-            if properties != None and not prop_name in properties:
-                continue
-            
-            try:
-                properties_text += f"{prop_name}: {objectParser.propertyToText(prop_value)}\n"
-            except:
-                print('==================')
-                print(prop_name)
-                print(prop_value)
-                print('==================')
-                pass
+def pageToText(page, isContainMetadata=False, properties = None ):
+    # Metadata
+    metadata_text = ""
+    if isContainMetadata:
+        metadata_text += f"Page URL: {page['url']}\n"
+        metadata_text += f"Created Time: {page['created_time']}\n"
+        metadata_text += f"Last Edited Time: {page['last_edited_time']}\n"
+        metadata_text += f"Created By: {page['created_by']['id']}\n"
+        metadata_text += f"Last Edited By: {page['last_edited_by']['id']}\n"
+        metadata_text += f"Archived: {page['archived']}\n"
+        metadata_text += f"Parent: {page['parent']['type']}\n"
+        metadata_text += f"Cover: {page['cover']['url']}\n" if page['cover'] != None else ""
 
-        return metadata_text + properties_text
+    properties_text = ""
+    for prop_name, prop_value in page['properties'].items():
+        if properties != None and not prop_name in properties:
+            continue    
+        properties_text += f"{prop_name}: {propertyToText(prop_value)}\n"
+    return metadata_text + properties_text
 
-    def databaseToText(block):
-        pass
+def databaseToText(block):
+    pass
